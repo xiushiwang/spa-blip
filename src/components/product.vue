d0c93de8b225cc46a02bba7e4d802ff429d5cc36
xsh <55852193+xiushiwang@users.noreply.github.com>
2020-10-15 00:59:28 -0500

    Update product.vue

--- src/components/product.vue
+++ src/components/product.vue
@@ -1,32 +1,357 @@
 <template>
   <div>
     <banner></banner>
-    <b-container>   
-        <h2>More about our product!</h2>
-    </b-container>                   
+    <b-container>
+      <!--start:Electricity rates vary throughout the day-->
+      <section class="real-estate-how-work">
+        <div class="container">
+          <div class="heading wow fadeInUp">
+            <div class="row">
+              <div class="col col-md-6">
+                <h3 class="text-36 leading-48">Electricity rates vary throughout the day</h3>
+              </div>
+              <div class="col col-md-6">
+                <p class="text-gray leading-32">
+                  Electricity is more expensive during “peak” hours - when many people return home and turn on lights and other devices.
+                </p>
+                <p class="text-gray leading-32">
+                  The costs are lower during “off-peak” hours - when most are at work, school, or sleeping.
+                </p>
+              </div>
+            </div>
+          </div>
 
+          <div class="row">
+            <div id="chartOne" style="width: 1000px;height: 400px;"></div>
+            <h4 class="text-20 text-black mb-12">blip stores electricity when it’s cheap</h4>
+          </div>
+        </div>
+      </section>
+      <!--end:Electricity rates vary throughout the day-->
+
+      <!--start:Don’t pay for high cost electricity-->
+      <section class="real-estate-how-work">
+        <div class="container">
+          <div class="heading wow fadeInUp">
+            <div class="row">
+              <div class="col col-md-6">
+                <h3 class="text-36 leading-48">Don’t pay for high cost electricity</h3>
+              </div>
+              <div class="col col-md-6">
+                <p class="text-gray leading-32">
+                  blip stores cheap electricity for use later in the day
+                </p>
+                <p class="text-gray leading-32">
+                  By using stored energy while the real time price of electricity is expensive, you save money on your electric bill
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="row">
+            <div id="chartTwo" style="width: 1000px;height: 400px;"></div>
+            <h4 class="text-20 text-black mb-12">blip saves you money when electricity is expensive</h4>
+          </div>
+        </div>
+      </section>
+      <!--end:Don’t pay for high cost electricity-->
+
+      <!--start:So why choose blip?-->
+      <section class="features">
+        <div class="container">
+
+          <div class="row">
+            <div class="col-lg-8 offset-lg-2 col-md-12 col-sm-12">
+              <div class="heading text-center wow fadeInUp">
+                <h1 class="text-black mt-16">So why choose blip?</h1>
+              </div>
+            </div>
+          </div>
+
+          <div class="content">
+            <div class="row">
+
+              <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="card-f flex text-center wow fadeInUp">
+                  <div class="card-f-heading">
+                    <div class="ic ic-lg ic-badge bg-primary-100 mb-36">
+                      <img src="../assets/blip-icons-product-portable.png" alt="">
+                    </div>
+                  </div>
+                  <div class="card-f-body">
+                    <h4 class="text-20 text-black mb-12">Portable</h4>
+                    <p class="text-gray mb-20">
+                      Move frequently? No problem. You can easily take blip with you to your next home.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="card-f flex text-center wow fadeInUp">
+                  <div class="card-f-heading">
+                    <div class="ic ic-lg ic-badge bg-primary-100 mb-36">
+                      <img src="../assets/blip-icons-product-installation.png" alt="">
+                    </div>
+                  </div>
+                  <div class="card-f-body">
+                    <h4 class="text-20 text-black mb-12">No installation required</h4>
+                    <p class="text-gray mb-20">
+                      blip plugs right into a wall outlet. No electricians necessary.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-lg-4 col-md-6 col-sm-12">
+                <div class="card-f flex text-center wow fadeInUp">
+                  <div class="card-f-heading">
+                    <div class="ic ic-lg ic-badge bg-primary-100 mb-36">
+                      <img src="../assets/blip-icons-product-emission.png" alt="">
+                    </div>
+                  </div>
+                  <div class="card-f-body">
+                    <h4 class="text-20 text-black mb-12">Reduce emissions</h4>
+                    <p class="text-gray mb-20">
+                      Some dirty power plants exist only to meet demand during peak hours.
+                      By lowering demand at peak times, these plants do not have to run.
+                    </p>
+                  </div>
+                </div>
+              </div>
+
+            </div>
+          </div>
+
+          <div class="card-pricing-footer">
+            <a href="./calculator" class="btn btn-outline-primary btn-block d-flex justify-between items-center">SEE HOW MUCH YOU WILL SAVE<i class="fas fa-arrow-right text-14"></i></a>
+          </div>
+
+        </div>
+      </section>
+      <!--end:So why choose blip?-->
+
+      <!--start:FAQs-->
+      <section id="career-detail" class="career-detail xl-pt-36 pb-120">
+        <div class="container">
+          <div class="col-12 col-sm-12 col-md-12 col-lg-7 offset-lg-1 col-xl-7 offset-xl-1">
+            <div class="pr-16">
+              <h1 class="text-hkgrotesk font-bold text-black mb-24 wow fadeInUp">FAQs</h1>
+
+              <h4 class="text-36 mb-12 mt-40 text-black wow fadeInUp">How much money can I expect to save?</h4>
+              <p class="text-gray wow fadeInUp">
+                It depends on where you live. blip can save you $50 a month or more. Check our
+                <a href="./calculator" class=""><u>savings calculator</u></a>
+                for more information!
+              </p>
+              <h4 class="text-36 mb-12 mt-40 text-black wow fadeInUp">Do I need an electrician to install it?</h4>
+              <p class="text-gray wow fadeInUp">
+                Nope! Just plug it into your outlet. You will then finish setting up your blip via your user dashboard.
+              </p>
+              <h4 class="text-36 mb-12 mt-40 text-black wow fadeInUp">How does blip help the environment?</h4>
+              <p class="text-gray wow fadeInUp">
+                Some dirty power plants exist only to meet demand during peak energy times.
+                By lowering demand at peak times, these plants won’t have to run as often.
+                When enough blip units are installed, utilities may be able to shut down these dirty power plants altogether.
+              </p>
+              <h4 class="text-36 mb-12 mt-40 text-black wow fadeInUp">I have an idea for blip. Who can I talk to?</h4>
+              <p class="text-gray wow fadeInUp">
+                We would love it if you filled out our survey! You can help shape the future of blip.
+                If you have have more ideas, please reach out by filling out the contact form.
+              </p>
+
+              <div class="content text-center">
+                <button type="button" class="btn btn-outline-primary" data-toggle="modal" data-target="#modalSignup">
+                  Take Our Survey
+                </button>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </section>
+      <!--end:FAQs-->
+
+      <!--start:Have more questions?-->
+      <section class="landing-agency-cta wow fadeInUp">
+        <div class="container">
+          <h3 class="wow fadeInUp">Have more questions?</h3>
+          <a href="https://www.blipenergy.com/contact-us" class="btn btn-outline-primary">CONTACT US<i class="fas fa-arrow-right text-14"></i></a>
+        </div>
+      </section>
+      <!--end:Have more questions?-->
+
+      <!--start:blip is launching soon-->
+      <div class="hero">
+        <div class="container">
+          <div class="row">
+            <div class="col-lg-8 offset-lg-2 col-md-12 col-sm-12">
+              <div class="content text-center">
+                <h2 class="text-white mb-24 wow fadeInUp">blip is launching soon</h2>
+                <h3 class="text-white mb-24 wow fadeInUp">get notified</h3>
+                <form>
+                  <p class="text-gray mt-8 mb-24">Name *</p>
+                  <div class="form-row">
+                    <div class="form-group col-md-6">
+                      <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="">
+                      <p class="text-gray mt-8 mb-24">First Name</p>
+                    </div>
+                    <div class="form-group col-md-6">
+                      <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="">
+                      <p class="text-gray mt-8 mb-24">Last Name</p>
+                    </div>
+                  </div>
+
+                  <div class="form-group">
+                    <p class="text-gray mt-8 mb-24">Email *</p>
+                    <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="">
+                  </div>
+
+                  <div class="form-group">
+                    <p class="text-gray mt-8 mb-24">Zip Code *</p>
+                    <input type="text" class="form-control" aria-describedby="emailHelp" placeholder="">
+                  </div>
+
+                  <div class="form-group mb-0">
+                    <button type="submit" class="btn btn-outline-primary">Get Started</button>
+                  </div>
+                </form>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!--end:blip is launching soon-->
+
+    </b-container>
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
 import banner from "./banner.vue";
+import Footer from "./Footer.vue";
+
+// import chart from './chart' //导入组件
+// import * as echarts from 'node_mpdules/echarts'
+
+import echarts from "echarts";
+require('echarts/theme/shine');
+// import ecStat from "echarts-stat";
+// import { setTimeout } from "timers";
 
 export default {
   name: "product",
   data() {
     return {
+      chartOne: null,
+      chartTwo: null
     };
   },
   mounted() {
-
-
+    this.$nextTick(function() {
+      this.drawChartOne('chartOne')
+    })
+    this.$nextTick(function() {
+      this.drawChartTwo('chartTwo')
+    })
   },
 
   methods: {
+    drawChartOne(id){
+      this.charts = echarts.init(document.getElementById(id))
+      // this.charts.setOption({
+      this.charts.setOption({
+        tooltip: {
+          trigger: 'axis',
+          // axisPointer: {
+          //   type: 'cross'
+          // }
+        },
+        legend: {
+          data: ['Utility Costs Throughout the Day']
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        xAxis: {
+          type: 'category',
+          axisLine: {onZero: false},
+          boundaryGap: false,
+          data: ['3 AM', '6 AM', '9 AM', '12 PM', '3 PM', '6 PM', '9 PM', '12 AM']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [{
+          name: 'Utility Costs Throughout the Day',
+          data: [1, 1, 2, 1.5, 2, 5, 2, 0],
+          type: 'line',
+          smooth: true,
+        }]
+      })
+
+    },
+
+    drawChartTwo(id){
+      this.charts = echarts.init(document.getElementById(id))
+      // this.charts.setOption({
+      this.charts.setOption({
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {
+          data: ['Utility Costs Throughout the Day', 'Lower Utility Costs with blip']
+        },
+        grid: {
+          left: '3%',
+          right: '4%',
+          bottom: '3%',
+          containLabel: true
+        },
+        toolbox: {
+          feature: {
+            saveAsImage: {}
+          }
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: ['3 AM', '6 AM', '9 AM', '12 PM', '3 PM', '6 PM', '9 PM', '12 AM']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            name: 'Utility Costs Throughout the Day',
+            data: [1, 1, 2, 1.5, 2, 5, 2, 0],
+            type: 'line',
+            smooth: true,
+          },
+          {
+            name: 'Lower Utility Costs with blip',
+            data: [1, 1, 1.5, 1, 1, 2, 2, 1],
+            type: 'line',
+            smooth: true,
+          }
+        ]
+      })
+    }
+
   },
   components: {
-    banner
-
+    banner,
+    echarts,
+    Footer
   }
 };
 </script>
