030e08ea629dd92169a233af7cafe5b2bf9247c4
xiushiwang <xiushiwang2020@u.northwestern.edu>
2021-06-30 21:01:41 -0500

    Updates on Jun 30th

--- src/app/website/product.vue
+++ src/app/website/product.vue
@@ -137,55 +137,55 @@
       <!--end:So why choose blip?-->
 
       <!--start:FAQs-->
-      <section class="FAQs oneD3755">
-        <div class="container">
-          <div class="w-100p">
-            <div class="pr-16">
-              <h2 class="h2 c-FFFFFF t-center">FAQs</h2>
+<!--      <section class="FAQs oneD3755">-->
+<!--        <div class="container">-->
+<!--          <div class="w-100p">-->
+<!--            <div class="pr-16">-->
+<!--              <h2 class="h2 c-FFFFFF t-center">FAQs</h2>-->
 
-              <div class="topline">
-                <h3 class="h3 c-B0E7FF t-left wow fadeInUp">How much money can I expect to save?</h3>
-                <p class="p2 c-FFFFFF t-left w-511px wow fadeInUp">
-                  It depends on where you live. blip can save you $50 a month or more. Check our
-                  <a href="./calculator" class=""><u>savings calculator</u></a>
-                  for more information!
-                </p>
-              </div>
+<!--              <div class="topline">-->
+<!--                <h3 class="h3 c-B0E7FF t-left wow fadeInUp">How much money can I expect to save?</h3>-->
+<!--                <p class="p2 c-FFFFFF t-left w-511px wow fadeInUp">-->
+<!--                  It depends on where you live. blip can save you $50 a month or more. Check our-->
+<!--                  <a href="./calculator" class=""><u>savings calculator</u></a>-->
+<!--                  for more information!-->
+<!--                </p>-->
+<!--              </div>-->
 
-              <div class="topline">
-                <h3 class="h3 c-B0E7FF t-left wow fadeInUp">Do I need an electrician to install it?</h3>
-                <p class="p2 c-FFFFFF t-left w-511px wow fadeInUp">
-                  Nope! Just plug it into your outlet. You will then finish setting up your blip via your user dashboard.
-                </p>
-              </div>
+<!--              <div class="topline">-->
+<!--                <h3 class="h3 c-B0E7FF t-left wow fadeInUp">Do I need an electrician to install it?</h3>-->
+<!--                <p class="p2 c-FFFFFF t-left w-511px wow fadeInUp">-->
+<!--                  Nope! Just plug it into your outlet. You will then finish setting up your blip via your user dashboard.-->
+<!--                </p>-->
+<!--              </div>-->
 
-              <div class="topline">
-                <h3 class="h3 c-B0E7FF t-left wow fadeInUp">How does blip help the environment?</h3>
-                <p class="p2 c-FFFFFF t-left w-511px wow fadeInUp">
-                  Some dirty power plants exist only to meet demand during peak energy times.
-                  By lowering demand at peak times, these plants won’t have to run as often.
-                  When enough blip units are installed, utilities may be able to shut down these dirty power plants altogether.
-                </p>
-              </div>
+<!--              <div class="topline">-->
+<!--                <h3 class="h3 c-B0E7FF t-left wow fadeInUp">How does blip help the environment?</h3>-->
+<!--                <p class="p2 c-FFFFFF t-left w-511px wow fadeInUp">-->
+<!--                  Some dirty power plants exist only to meet demand during peak energy times.-->
+<!--                  By lowering demand at peak times, these plants won’t have to run as often.-->
+<!--                  When enough blip units are installed, utilities may be able to shut down these dirty power plants altogether.-->
+<!--                </p>-->
+<!--              </div>-->
 
-              <div class="topline">
-                <h3 class="h3 c-B0E7FF t-left wow fadeInUp">I have an idea for blip. Who can I talk to?</h3>
-                <p class="p2 c-FFFFFF t-left w-511px wow fadeInUp">
-                  Please reach out to us by filling out the contact form.
-                </p>
-              </div>
+<!--              <div class="topline">-->
+<!--                <h3 class="h3 c-B0E7FF t-left wow fadeInUp">I have an idea for blip. Who can I talk to?</h3>-->
+<!--                <p class="p2 c-FFFFFF t-left w-511px wow fadeInUp">-->
+<!--                  Please reach out to us by filling out the contact form.-->
+<!--                </p>-->
+<!--              </div>-->
 
-              <div class="content t-center">
-                <p class="p2 c-FFFFFF t-center wow fadeInUp">Have more questions?</p>
-                <button type="button" class="btn btn-outline-B0E7FF" data-toggle="modal" data-target="#modalSignup" onclick="window.location.href='./contact-us'">
-                  Contact Us
-                </button>
-              </div>
+<!--              <div class="content t-center">-->
+<!--                <p class="p2 c-FFFFFF t-center wow fadeInUp">Have more questions?</p>-->
+<!--                <button type="button" class="btn btn-outline-B0E7FF" data-toggle="modal" data-target="#modalSignup" onclick="window.location.href='./contact-us'">-->
+<!--                  Contact Us-->
+<!--                </button>-->
+<!--              </div>-->
 
-            </div>
-          </div>
-        </div>
-      </section>
+<!--            </div>-->
+<!--          </div>-->
+<!--        </div>-->
+<!--      </section>-->
     </div>
     <Footer></Footer>
   </div>
